16190bed0f5fc3fb78ddeab85c3d9753307e0b71
Powerxurrem <133252385+Powerxurrem@users.noreply.github.com>
2026-02-01 14:56:47 +0100

    basic protection

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,37 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-  const auth = req.headers.get('authorization');
-
-  const user = process.env.BASIC_AUTH_USER;
-  const pass = process.env.BASIC_AUTH_PASS;
+  const auth = req.headers.get('authorization')
 
   if (!auth) {
-    return new NextResponse('Authentication required', {
+    return new Response('Auth required', {
       status: 401,
       headers: {
         'WWW-Authenticate': 'Basic realm="Protected"',
       },
-    });
+    })
   }
 
-  const [, encoded] = auth.split(' ');
-  const decoded = Buffer.from(encoded, 'base64').toString();
-  const [u, p] = decoded.split(':');
+  const [, encoded] = auth.split(' ')
+  const decoded = Buffer.from(encoded, 'base64').toString()
+  const [user, pass] = decoded.split(':')
 
-  if (u !== user || p !== pass) {
-    return new NextResponse('Invalid credentials', {
+  if (
+    user !== process.env.BASIC_AUTH_USER ||
+    pass !== process.env.BASIC_AUTH_PASS
+  ) {
+    return new Response('Invalid credentials', {
       status: 401,
       headers: {
         'WWW-Authenticate': 'Basic realm="Protected"',
       },
-    });
+    })
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: [
-    '/',        // homepage
-    '/about',
-    '/pricing',
-    '/trips',
-    '/mixer',
-    '/autopilot',
-  ],
-};
+  matcher: ['/:path*'],
+}
